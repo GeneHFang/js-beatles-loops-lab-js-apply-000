a560af661048d1dc028ef4fde40e3e25a89c39a5
Gene H Li <ghl234@nyu.edu>
2019-08-12 21:33:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(array_musicians, array_instr)
 {
   var local_array = [];
-  for(i = 0; i < array_musicians.length ; i++ )
+  for(var i = 0; i < array_musicians.length ; i++ )
   {
     var temp = array_musicians[i];
     var temp2 = " plays ";
